707a8f4a279bef0372ae7f3a99f29099558ff43b
davidcoats <david@getsafetycone.com>
2023-08-12 23:24:02 -0700

    Interim changes.

---
+++ source/R5T.T0154/Code/Attributes/RazorLayoutMarkerAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using R5T.T0143;
+
+
+namespace R5T.T0154
+{
+    /// <summary>
+    /// Attribute indicating a type is a Razor layout.
+    /// It's usually pretty clear that something is a Razor layout, but the marker attribute is useful for surveying assemblies and code, then building a catalogue of Razor layouts.
+    /// </summary>
+    [AttributeUsage(
+        // Only class, since Razor components are only classes.
+        AttributeTargets.Class,
+        AllowMultiple = false, Inherited = false)]
+    [MarkerAttributeMarker]
+    public class RazorLayoutMarkerAttribute : Attribute,
+        IMarkerAttributeMarker
+    {
+        /// <summary>
+        /// Allows specifying that a type is *not* a Razor layout type.
+        /// This is useful for marking types that end up canonical Razor layout code file locations, but are not actually Razor layouts.
+        /// </summary>
+        public bool IsRazorLayout { get; }
+
+
+        public RazorLayoutMarkerAttribute(
+            bool isRazorLayout = true)
+        {
+            this.IsRazorLayout = isRazorLayout;
+        }
+    }
+}
---
+++ source/R5T.T0154/Code/Attributes/RazorPageMarkerAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using R5T.T0143;
+
+
+namespace R5T.T0154
+{
+    /// <summary>
+    /// Attribute indicating a type is a Razor page.
+    /// It's usually pretty clear that something is a Razor page, but the marker attribute is useful for surveying assemblies and code, then building a catalogue of Razor pages.
+    /// <para>
+    /// Note! The page marker is <strong>only</strong> for pages that are URL-reachable (usually only Razor components with a page-directive).
+    /// </para>
+    /// </summary>
+    [AttributeUsage(
+        // Only class, since Razor components are only classes.
+        AttributeTargets.Class,
+        AllowMultiple = false, Inherited = false)]
+    [MarkerAttributeMarker]
+    public class RazorPageMarkerAttribute : Attribute,
+        IMarkerAttributeMarker
+    {
+        /// <summary>
+        /// Allows specifying that a type is *not* a Razor page type.
+        /// This is useful for marking types that end up canonical Razor page code file locations, but are not actually Razor pages.
+        /// </summary>
+        public bool IsRazorPage { get; }
+
+
+        public RazorPageMarkerAttribute(
+            bool isRazorPage = true)
+        {
+            this.IsRazorPage = isRazorPage;
+        }
+    }
+}
